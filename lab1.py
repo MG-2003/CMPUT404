64e5d337b32a2cfb94155cc70e4142ea9758b2f7
MG-2003 <mgadhia@ualberta.ca>
2023-01-11 18:47:19 -0700

    update python script

--- lab1.py
+++ lab1.py
@@ -3,5 +3,6 @@ import requests
 print(requests.__version__)
 
 response = requests.get("http://www.google.com/")
-
+response2 = requests.get("https://raw.githubusercontent.com/MG-2003/CMPUT404/main/lab1.py")
 print(response.text)
+print(response2.text)
